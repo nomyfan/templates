a451c8d3323add24a3bb0b8b5ebf527a0452ac06
nomyfan <Nomyfan@live.com>
2023-04-16 21:28:16 +0800

    Add tailwindcss for vite-react-ts

--- vite-react-ts/src/App.tsx
+++ vite-react-ts/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>vite-react-ts</div>;
+  return (
+    <div className="text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-amber-300 to-red-500 text-center">
+      vite-react-ts
+    </div>
+  );
 }
 
 export default App;
